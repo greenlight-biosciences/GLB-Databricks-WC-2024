e6bf90c2e422c8cdbb02c9d6a75108071c5c3d50
Mihir Kestur <mihir.kestur@greenlightbio.com>
2024-10-08 11:17:08 -0400

    DTS-1498 #comment Change config

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 from langchain_openai import AzureChatOpenAI
 from dotenv import load_dotenv
 import os
-load_dotenv()
+load_dotenv(override=True)
 
 # Initialize the AzureOpenAI class with the model name
 lc_llm = AzureChatOpenAI(
-    model_name=os.environ["AZURE_OPENAI_DEPLOYMENT"],
-    azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
-    deployment_name=os.environ["AZURE_OPENAI_DEPLOYMENT"],
-    openai_api_key=os.environ["AZURE_OPENAI_KEY"],
-    openai_api_version=os.environ["AZURE_OPENAI_VERSION"],
+    model_name=os.getenv["AZURE_OPENAI_DEPLOYMENT"],
+    azure_endpoint=os.getenv["AZURE_OPENAI_ENDPOINT"],
+    deployment_name=os.getenv["AZURE_OPENAI_DEPLOYMENT"],
+    openai_api_key=os.getenv["AZURE_OPENAI_KEY"],
+    openai_api_version=os.getenv["AZURE_OPENAI_VERSION"],
 )
