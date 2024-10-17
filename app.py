1eca664579950cfd2f1bce3b87c2545226ac62c1
Mihir Kestur <mihir.kestur@greenlightbio.com>
2024-10-17 11:01:26 -0400

    DTS-1498 #comment Update app.py

--- app.py
+++ app.py
@@ -1,54 +1,55 @@
 import logging
 import os
+import uuid
 import streamlit as st
 from databricks.sdk import WorkspaceClient
 from databricks.sdk.service.serving import ChatMessage, ChatMessageRole
 import base64
 from langchain_core.messages import HumanMessage, AIMessage
-from src.ImgProcessingAgent import agent
-
-# Set up logging
+from ImgProcessingAgent import agent
+from helper import push_data, dbrx_vs
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-
-def get_user_info():
-    headers = st.context.headers
-    return dict(
-        user_name=headers.get("X-Forwarded-Preferred-Username"),
-        user_email=headers.get("X-Forwarded-Email"),
-        user_id=headers.get("X-Forwarded-User"),
-    )
-
-user_info = get_user_info()
+st.set_page_config(page_title="AdaptiveFilters", layout="wide")
+os.environ['DATABRICKS_HOST'] = "https://dbc-8814f776-f435.cloud.databricks.com"
 
 # Streamlit app
 if "visibility" not in st.session_state:
     st.session_state.visibility = "visible"
     st.session_state.disabled = False
 
-st.title("GreenLight Databricks PoC")
+st.title("AdaptiveFilters: Image Processing Agent")
 
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
+    st.session_state.plan_json = dict()
+    st.session_state.original_image = None
     st.session_state.current_image = None
+    st.session_state.lg_msg = []
+    st.session_state.db_msg = []
 
 if new_img := st.sidebar.file_uploader("Choose an image...", type=["png"], label_visibility=st.session_state.visibility):
-    st.session_state.current_image = base64.b64encode(new_img.read()).decode("utf-8")
+    st.session_state.original_image = base64.b64encode(new_img.read()).decode("utf-8")
+    st.session_state.current_image = st.session_state.original_image
     new_img = None
-if st.session_state.current_image is not None:
-    st.sidebar.image(f'data:image/png;base64,{st.session_state.current_image}', caption='Current Image')
+
+if st.session_state.original_image is not None:
+    st.sidebar.image(f'data:image/png;base64,{st.session_state.original_image}', caption='Original Image')
 
 chat_area, finetune = st.columns([4,1])
 
 # Accept user input
-if prompt := st.chat_input("How can i process your image?", disabled=(st.session_state.current_image==None)):
+if prompt := st.chat_input("How can I process your image?", disabled=(st.session_state.current_image is None)):
     
+    st.session_state.db_msg.append(ChatMessage(role=ChatMessageRole.USER, content=prompt))
     input_message = HumanMessage(content=prompt)
     st.session_state.messages.append(input_message)
 
     # NOTE: Add Streamlit progress
-    st.session_state.current_image, plan_json = agent.process_image(prompt, st.session_state.current_image)
+    with st.spinner("Processing Image"):
+        st.session_state.current_image, st.session_state.plan_json = agent.process_image(prompt, st.session_state.current_image, st.session_state.plan_json)
+        
     content = [
         {"type": "text", "text": "Processed Image"},
         {
@@ -56,11 +57,12 @@ if prompt := st.chat_input("How can i process your image?", disabled=(st.session
             "image_url": {"url": f"data:image/png;base64,{st.session_state.current_image}"},
         },
         {
-         "type":"json", "data":plan_json
+         "type":"json", "data":st.session_state.plan_json
         }
     ]
     input_message = AIMessage(content=content)
     st.session_state.messages.append(input_message)
+    # st.session_state.db_msg.append(ChatMessage(role=ChatMessageRole.SYSTEM, content=content))
     st.rerun()
 
 # Display chat messages from history on app rerun
@@ -95,10 +97,12 @@ for i in range(len(st.session_state.messages)):
                 unsafe_allow_html=True
             )
             if i == len(st.session_state.messages)-1:
+                ftr, rsn = st.columns(2)
                 filters = msg.content[2]['data']['filters']
+                planner_reason = msg.content[2]['data']['planner_reason']
                 # Generate sliders dynamically
                 for filter_item in filters:
-                    st.markdown(f"<div class='centered'>{filter_item['name'].capitalize()} Filter</div>", unsafe_allow_html=True)
+                    ftr.markdown(f"<div class='centered'>{filter_item['name'].capitalize()} Filter</div>", unsafe_allow_html=True)
                     
                     for key, value in filter_item.items():
                         if key == 'name': continue
@@ -106,30 +110,13 @@ for i in range(len(st.session_state.messages)):
                             if isinstance(value, list):
                                 for j, v in enumerate(value):
                                     keyName = key.capitalize()+f"[{j}]"
-                                    filter_item[key][j] = st.slider(keyName, 0, 255, v, key=keyName+f"-{i}")
+                                    filter_item[key][j] = ftr.slider(keyName, 0, 255, v, key=keyName+f"-{i}{str(uuid.uuid4())}")
                             elif isinstance(value, int):
-                                filter_item[key] = st.slider(key.capitalize(), 1, 50, value, step=2, key=f"{i}")
+                                filter_item[key] = ftr.slider(key.capitalize(), 1, 50, value, step=2, key=f"{i}{str(uuid.uuid4())}")
 
                 # Display updated filters
-                st.sidebar.write("Updated Filters:", filters)
+                rsn.write(planner_reason)
                 changed_img = agent._apply_filters(st.session_state.current_image, filters)
-                st.sidebar.image(f'data:image/png;base64,{changed_img}')
-
-# Initialize the Databricks Workspace Client
-# w = WorkspaceClient()
-
-# Ensure environment variable is set correctly
-# assert os.getenv('SERVING_ENDPOINT'), "SERVING_ENDPOINT must be set in app.yaml."
-# # Display assistant response in chat message container
-# with st.chat_message("assistant"):
-#     # Query the Databricks serving endpoint
-#     try:
-#         response = w.serving_endpoints.query(
-#             name=os.getenv("SERVING_ENDPOINT"),
-#             messages=messages,
-#             max_tokens=400,
-#         )
-#         assistant_response = response.choices[0].message.content
-#         st.markdown(assistant_response)
-#     except Exception as e:
-#         st.error(f"Error querying model: {e}")
+                rsn.image(f'data:image/png;base64,{changed_img}', caption='Updated Image')
+                if st.button("Store this pipeline!", use_container_width=True, key=str(uuid.uuid4())):
+                    push_data(filters, planner_reason)
